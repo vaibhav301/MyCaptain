2375fbec31c13929f129b596ec1f67fcd36e5830
vaibhav301 <60523994+vaibhav301@users.noreply.github.com>
2020-08-06 18:16:20 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+int a;
+char b;
+float c;
+double d;
+cout<<"size of int:"<<sizeof(a)<<"bytes"<<endl;
+cout<<"size of char:"<<sizeof(a)<<"bytes"<<endl;
+cout<<"size of int:"<<sizeof(b)<<"bytes"<<endl;
+cout<<"size of float:"<<sizeof(c)<<"bytes"<<endl;
+cout<<"size of double:"<<sizeof(d)<<"bytes"<<endl;
+retuen 0;
+}
