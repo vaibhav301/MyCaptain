88b3c646db12817ba67b2b7387045e5663ad701b
vaibhav301 <60523994+vaibhav301@users.noreply.github.com>
2020-08-06 18:30:24 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -7,10 +7,10 @@ int a;
 char b;
 float c;
 double d;
-cout<<"size of int:"<<sizeof(a)<<"bytes"<<endl;
-cout<<"size of char:"<<sizeof(a)<<"bytes"<<endl;
-cout<<"size of int:"<<sizeof(b)<<"bytes"<<endl;
-cout<<"size of float:"<<sizeof(c)<<"bytes"<<endl;
-cout<<"size of double:"<<sizeof(d)<<"bytes"<<endl;
+cout<<"size of int:"<<sizeof(a)<<" bytes"<<endl;
+cout<<"size of char:"<<sizeof(a)<<" bytes"<<endl;
+cout<<"size of int:"<<sizeof(b)<<" bytes"<<endl;
+cout<<"size of float:"<<sizeof(c)<<" bytes"<<endl;
+cout<<"size of double:"<<sizeof(d)<<" bytes"<<endl;
 retuen 0;
 }
