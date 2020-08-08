2b385eec986c06ae173f18152322a335880eb070
vaibhav301 <60523994+vaibhav301@users.noreply.github.com>
2020-08-09 00:25:20 +0530

    Create Project 3.cpp

---
+++ Project 3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+int arr[5];
+cout<<"Enter Elements:"<<endl;
+for(int i=0;i<=5;i++)
+{
+cin>>arr[i];
+}
+cout<<"you entered:";
+for(int i=0;i<=5;i++)
+{
+cout<<arr[i]<<endl;
+}
+return 0;
+}
+
+
+
