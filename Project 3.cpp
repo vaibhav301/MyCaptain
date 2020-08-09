6ce520875d2da37f491f1e639e04ecd588559c4e
vaibhav301 <60523994+vaibhav301@users.noreply.github.com>
2020-08-09 11:50:51 +0530

    Update Project 3.cpp

--- Project 3.cpp
+++ Project 3.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int main()
 {
-int arr[5];
+int arr[4];
 cout<<"Enter Elements:"<<endl;
-for(int i=0;i<=5;i++)
+for(int i=0;i<=4;i++)
 {
 cin>>arr[i];
 }
 cout<<"you entered:";
-for(int i=0;i<=5;i++)
+for(int i=0;i<=4;i++)
 {
 cout<<arr[i]<<endl;
 }
