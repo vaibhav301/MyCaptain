a835afd5f4a99a2f05dca1f91559c64afe946635
vaibhav301 <60523994+vaibhav301@users.noreply.github.com>
2020-08-31 18:31:17 +0530

    Create function.cpp

---
+++ function.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+bool checkPrime(int n);
+
+int main() {
+    int n, i;
+    bool flag = false;
+
+    cout << "Enter a positive  integer: ";
+    cin >> n;
+
+    for(i = 2; i <= n/2; ++i) {
+        if (checkPrime(i)) {
+            if (checkPrime(n - i)) {
+                cout << n << " = " << i << " + " << n-i << endl;
+                flag = true;
+            }
+        }
+    }
+
+    if (!flag)
+      cout << n << " can't be expressed as sum of two prime numbers.";
+
+    return 0;
+}
+
+bool checkPrime(int n)
+{
+    int i;
+    bool isPrime = true;
+
+    if (n == 0 || n == 1) {
+        isPrime = false;
+    }
+    else {
+        for(i = 2; i <= n/2; ++i) {
+            if(n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    return isPrime;
+}
